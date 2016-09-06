ccc9718d35c2a2f0c73b27d9b8d7785e5d128f19
Shaul Hameed <shaul@calibraint.com>
2016-09-06 12:55:15 +0530

    Initial Commit

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by cloudmaxis on 06/09/16.
+//  Copyright © 2016 cloudmaxis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by cloudmaxis on 06/09/16.
+//  Copyright © 2016 cloudmaxis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
