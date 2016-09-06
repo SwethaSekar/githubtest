e1436c165764ed426077e4c2146ec8f3cc2e4c6b
Shaul Hameed <shaul@calibraint.com>
2016-09-06 18:14:24 +0530

    loginpage

--- githubtest/ViewController.h
+++ githubtest/ViewController.h
@@ -9,7 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    NSURLSession *session;
+}
 
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UITextField *text1;
+@property (weak, nonatomic) IBOutlet UITextField *text2;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+
+-(IBAction)click1:(id)sender;
 
 @end
 
--- githubtest/ViewController.m
+++ githubtest/ViewController.m
@@ -8,13 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSURLSessionDataDelegate>
 
 @end
 
 @implementation ViewController
-
+@synthesize label1,label2,button1,button2,button3;
 - (void)viewDidLoad {
+    label1.text = @"Username";
+    label2.text = @"Password";
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -23,5 +26,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(IBAction)click1:(id)sender
+{
+    
+       NSString *string = @"https://api.github.com/users/";
+   NSString *str1 = [string stringByAppendingString:_text1.text];
+        NSURL *url = [NSURL URLWithString:str1];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        NSLog(@"dict-->%@",dict);
+        //NSString *errstr = [dict valueForKey: @"message"];
+        if (dict[@"message"])
+        {
+//            NSLog(@"hai");
+//           UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:@"Not Found" preferredStyle:UIAlertControllerStyleAlert];
+//           
+//           UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];           [alertController addAction:ok];
+//           
+//           [self presentViewController:alertController animated:YES completion:nil];
+//                   }
+//        
+//            NSLog(@"hello");
+            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"hai" message:@"hello" delegate:self cancelButtonTitle:@"ok" otherButtonTitles:@"cancel", nil];
+            [alert show];
+        }
+        
+      
+   
+                }];
+  
+   [task resume];
+   
+    
+
+}
+
 
 @end
